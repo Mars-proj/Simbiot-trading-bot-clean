599985d64e67a2e83a0066e97bc92a0fe0160b3d
simbiot <svobodg@gmail.com>
2025-04-10 04:01:42 +0000

    Remove unused Monitoring import from start_trading_all.py to fix ImportError

--- start_trading_all.py
+++ start_trading_all.py
@@ -6,7 +6,6 @@ from ml_predictor import Predictor
 from retraining_manager import RetrainingManager
 from queue_manager import QueueManager
 from notification_manager import NotificationManager
-from monitoring import Monitoring
 from risk_manager import RiskManager
 from signal_blacklist import SignalBlacklist
 from market_state_analyzer import MarketStateAnalyzer
@@ -32,7 +31,6 @@ async def start_trading_all(users, credentials, since, limit, timeframe, symbol_
     retraining_manager = RetrainingManager()
     queue_manager = QueueManager()
     notification_manager = NotificationManager()
-    monitoring = Monitoring()
     risk_manager = RiskManager()
     signal_blacklist = SignalBlacklist()
     market_state_analyzer = MarketStateAnalyzer()
